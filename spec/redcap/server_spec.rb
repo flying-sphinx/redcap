765776230ffa34cd46efa245045a9bfc2ca3fd01
Pat Allan <pat@freelancing-gods.com>
2017-05-12 21:23:05 +1000

    Update RSpec syntax.

--- spec/redcap/server_spec.rb
+++ spec/redcap/server_spec.rb
@@ -3,32 +3,32 @@ require 'spec_helper'
 describe Redcap::Server do
   describe '#receive_line' do
     let(:server) { Redcap::Server.new :spec }
-    let(:logger) { stub('logger', :info => nil) }
-    
+    let(:logger) { double('logger', :info => nil) }
+
     before :each do
       server.logger = logger
-      server.stub!(
+      allow(server).to receive_messages(
         :`                              => '24',
         :close_connection_after_writing => nil,
         :send_data                      => nil
       )
     end
-    
+
     it "sends the process id for a given port" do
-      server.should_receive(:send_data).with('24')
-      
+      expect(server).to receive(:send_data).with('24')
+
       server.receive_line('101')
     end
-    
+
     it "closes the connection once writing is finished" do
-      server.should_receive(:close_connection_after_writing)
-      
+      expect(server).to receive(:close_connection_after_writing)
+
       server.receive_line('101')
     end
-    
+
     it "logs the request" do
-      logger.should_receive(:info).with('port: 101 -> pid: 24')
-      
+      expect(logger).to receive(:info).with('port: 101 -> pid: 24')
+
       server.receive_line('101')
     end
   end
--- spec/redcap_spec.rb
+++ spec/redcap_spec.rb
@@ -2,20 +2,22 @@ require 'spec_helper'
 
 describe Redcap do
   describe '.pid_for_port' do
-    let(:socket) { stub('socket', :write => nil, :read => '42', :close => nil) }
-    
+    let(:socket) {
+      double('socket', :write => nil, :read => '42', :close => nil)
+    }
+
     before :each do
-      TCPSocket.stub! :new => socket
+      allow(TCPSocket).to receive(:new).and_return(socket)
     end
-    
+
     it "sends the given port to the socket" do
-      socket.should_receive(:write).with("101\n")
-      
+      expect(socket).to receive(:write).with("101\n")
+
       Redcap.pid_for_port(101)
     end
-    
+
     it "returns the socket output as an integer" do
-      Redcap.pid_for_port(101).should == 42
+      expect(Redcap.pid_for_port(101)).to eq(42)
     end
   end
 end
